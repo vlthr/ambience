6a717f14cf724c1ebd161a29b1d97a518f3a80e5
Valthor Halldorsson <halldorsson.v@gmail.com>
2020-04-09 20:12:51 +0200

    refactor: explicitly thread local version using Rc

--- src/thread.rs
+++ src/thread.rs
@@ -2,7 +2,7 @@ use crate::error::*;
 use std::any::Any;
 use std::any::TypeId;
 use std::cell::RefCell;
-use std::{collections::HashMap, marker::PhantomData, sync::Arc};
+use std::{collections::HashMap, marker::PhantomData, rc::Rc};
 
 thread_local! {
     static THREAD_LOCALS: RefCell<AmbientMap> = RefCell::new(AmbientMap::new());
@@ -18,7 +18,7 @@ struct AmbientMap {
 
 #[derive(Debug)]
 struct Slot {
-    ptr: Arc<dyn Any + 'static + Send + Sync>,
+    ptr: Rc<dyn Any + 'static>,
     id: usize,
 }
 
@@ -29,9 +29,9 @@ impl AmbientMap {
             id_counter: 0,
         }
     }
-    /// Returns a (cloned) Arc to the topmost (most recent still-active) value of type `T`.
+    /// Returns a (cloned) Rc to the topmost (most recent still-active) value of type `T`.
     /// Returns `None` if no values are stored for the given `T`.
-    pub fn peek<T: 'static + Send + Sync>(&self) -> Option<Arc<T>> {
+    pub fn peek<T: 'static>(&self) -> Option<Rc<T>> {
         self.data.get(&TypeId::of::<T>()).and_then(|stack| {
             if stack.len() > 0 {
                 stack[stack.len() - 1].ptr.clone().downcast::<T>().ok()
@@ -49,7 +49,7 @@ impl AmbientMap {
             .retain(|slot| &slot.id != id);
     }
     /// Adds a new value of type `T` to the top of the stack.
-    pub fn push<T: 'static + Send + Sync>(&mut self, new_val: Arc<T>) -> AmbientGuard<T> {
+    pub fn push<T: 'static>(&mut self, new_val: Rc<T>) -> AmbientGuard<T> {
         let type_id = TypeId::of::<T>();
         let new_id = self.id_counter;
         self.id_counter += 1;
@@ -90,7 +90,7 @@ where
 
 /// Retrieves thread-local ambient data of type `T` that was previously set on the **same** thread.
 /// The returned value will always be the most recently `set` value whose `AmbientGuard` still exists.
-pub fn get<T: 'static + Send + Sync>() -> Result<Arc<T>> {
+pub fn get<T: 'static>() -> Result<Rc<T>> {
     THREAD_LOCALS.with(|frame_opt| {
         frame_opt
             .borrow()
@@ -99,7 +99,6 @@ pub fn get<T: 'static + Send + Sync>() -> Result<Arc<T>> {
     })
 }
 
-/// TODO
 fn unset(type_id: &TypeId, id: &usize) {
     THREAD_LOCALS.with(|frame_opt| {
         let mut storage = frame_opt.borrow_mut();
@@ -113,28 +112,59 @@ fn unset(type_id: &TypeId, id: &usize) {
 /// `set` can be called multiple times. The values of type `T` are treated as a stack, where
 /// a call to `get::<T>()` will always yield the most recently added `T`. When the `AmbientGuard`
 /// for a value on the stack is dropped, it is removed from the stack.
-/// TODO: If the guard is manually dropped out-of-order, it is NOT guaranteed that the correct value is removed.
-pub fn set<T: 'static + Send + Sync>(new_val: Arc<T>) -> AmbientGuard<T> {
+pub fn set<T: 'static>(new_val: T) -> AmbientGuard<T> {
+    THREAD_LOCALS.with(|frame_opt| {
+        let mut storage = frame_opt.borrow_mut();
+        storage.push(Rc::new(new_val))
+    })
+}
+
+/// Sets thread-local ambient data of type `T`. Works like `set`, except the given `Rc` will be
+/// reused internally. Useful in cases where you already have a `Rc` and don't want to clone its contents.
+pub fn set_rc<T: 'static>(new_val: Rc<T>) -> AmbientGuard<T> {
     THREAD_LOCALS.with(|frame_opt| {
         let mut storage = frame_opt.borrow_mut();
         storage.push(new_val)
     })
 }
 
+/// ```compile_fail
+/// use std::sync::Arc;
+/// let one = 1u64;
+/// let outer_frame_guard = ambience::thread::set(&one);
+/// ```
+fn _data_must_be_static() {}
+
+/// ```compile_fail
+/// use ambience::thread::AmbientGuard;
+/// fn assert_send<T: Send>() {}
+///
+/// assert_send::<AmbientGuard<u64>>();
+/// ```
+fn _guard_is_send() {}
+
+/// ```compile_fail
+/// use ambience::thread::AmbientGuard;
+/// fn assert_sync<T: Sync>() {}
+///
+/// assert_sync::<AmbientGuard<u64>>();
+/// ```
+fn _guard_is_sync() {}
+
 #[cfg(test)]
 mod tests {
     use crate::error::*;
     use crate::thread as ambience;
     use std::any::Any;
-    use std::sync::Arc;
+    use std::rc::Rc;
 
     #[test]
     fn simple() -> Result<()> {
-        let one = Arc::new(1u64);
+        let one = 1u64;
         {
             // set `one` as ambient data
-            let _frame_guard = ambience::set(one.clone());
-            assert_eq!(ambience::get::<u64>().unwrap(), one);
+            let _frame_guard = ambience::set(one);
+            assert_eq!(*ambience::get::<u64>().unwrap(), one);
         }
         // no ambient frame in scope
         assert!(ambience::get::<u64>().is_err());
@@ -143,19 +173,19 @@ mod tests {
 
     #[test]
     fn nested() -> Result<()> {
-        let one = Arc::new(1u64);
-        let two = Arc::new(2u64);
+        let one = 1u64;
+        let two = 2u64;
         {
             // set `one` as ambient data
             let _frame_guard = ambience::set(one.clone());
-            assert_eq!(ambience::get::<u64>().unwrap(), one);
+            assert_eq!(*ambience::get::<u64>().unwrap(), one);
             {
                 // set `two` as ambient data in the inner scope
                 let _frame_guard = ambience::set(two.clone());
-                assert_eq!(ambience::get::<u64>().unwrap(), two);
+                assert_eq!(*ambience::get::<u64>().unwrap(), two);
             }
             // ambient data should be back to `one` after inner scope is dropped
-            assert_eq!(ambience::get::<u64>().unwrap(), one);
+            assert_eq!(*ambience::get::<u64>().unwrap(), one);
         }
         // no ambient frame in scope
         assert!(ambience::get::<u64>().is_err());
@@ -164,13 +194,13 @@ mod tests {
 
     #[test]
     fn types_are_independent() -> Result<()> {
-        let u_64 = Arc::new(64u64);
-        let u_32 = Arc::new(32u32);
+        let u_64 = 64u64;
+        let u_32 = 32u32;
         {
-            let _frame_guard = ambience::set(u_32.clone());
-            let _frame_guard = ambience::set(u_64.clone());
-            assert_eq!(ambience::get::<u64>().unwrap(), u_64);
-            assert_eq!(ambience::get::<u32>().unwrap(), u_32);
+            let _frame_guard = ambience::set(u_32);
+            let _frame_guard = ambience::set(u_64);
+            assert_eq!(*ambience::get::<u64>().unwrap(), u_64);
+            assert_eq!(*ambience::get::<u32>().unwrap(), u_32);
         }
         // no ambient frame in scope
         assert!(ambience::get::<u64>().is_err());
@@ -179,20 +209,33 @@ mod tests {
     }
 
     #[test]
-    fn manually_dropped() -> Result<()> {
-        let one = Arc::new(1u64);
-        let two = Arc::new(2u64);
+    fn external_rc_can_be_reused() -> Result<()> {
+        let one = Rc::new(1u64);
         {
             // set `one` as ambient data
-            let outer_frame_guard = ambience::set(one.clone());
-            assert_eq!(ambience::get::<u64>().unwrap(), one);
+            let _frame_guard = ambience::set_rc(one.clone());
+            assert_eq!(*ambience::get::<u64>().unwrap(), *one);
+        }
+        // no ambient frame in scope
+        assert!(ambience::get::<u64>().is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn manually_dropped() -> Result<()> {
+        let one = 1u64;
+        let two = 2u64;
+        {
+            // set `one` as ambient data
+            let outer_frame_guard = ambience::set(one);
+            assert_eq!(*ambience::get::<u64>().unwrap(), one);
             {
                 // set `two` as ambient data in the inner scope
-                let _frame_guard = ambience::set(two.clone());
+                let _frame_guard = ambience::set(two);
                 // dropping `outer_frame_guard` should remove `one` from the stack
                 drop(outer_frame_guard);
                 // `one` has been dropped, `two` should still be top of the stack
-                assert_eq!(ambience::get::<u64>().unwrap(), two);
+                assert_eq!(*ambience::get::<u64>().unwrap(), two);
             }
             // `outer_frame_guard` was manually dropped, so after leaving the inner scope
             // there should be nothing left on the stack
@@ -205,33 +248,9 @@ mod tests {
 
     #[test]
     fn downcast_expectations() -> Result<()> {
-        let arc_any: Arc<dyn Any + 'static + Send + Sync> = Arc::new(1u64);
+        let arc_any: Rc<dyn Any + 'static> = Rc::new(1u64);
         assert!(arc_any.clone().downcast::<u64>().is_ok());
-        assert!(arc_any.clone().downcast::<Arc<u64>>().is_err());
+        assert!(arc_any.clone().downcast::<Rc<u64>>().is_err());
         Ok(())
     }
-
-    #[test]
-    /// ```compile_fail
-    /// let one = 1u64;
-    /// let one = Arc::new(&one);
-    /// let outer_frame_guard = ambience::set(one.clone());
-    /// ```
-    fn data_must_be_static() {}
-
-    #[test]
-    /// ```compile_fail
-    /// fn assert_send<T: Send>() {}
-    ///
-    /// assert_send<AmbientGuard<u64>>();
-    /// ```
-    fn guard_is_not_send() {}
-
-    #[test]
-    /// ```compile_fail
-    /// fn assert_sync<T: Sync>() {}
-    ///
-    /// assert_sync<AmbientGuard<u64>>();
-    /// ```
-    fn guard_is_not_sync() {}
 }
